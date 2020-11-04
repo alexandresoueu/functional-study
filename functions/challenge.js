889834339b30b88b4b7797830da6b068c5b3beb8
calexandredev <calexandredev@gmail.com>
2020-11-04 07:44:07 -0300

    challenge DONE

--- functions/challenge.js
+++ functions/challenge.js
@@ -1,3 +1,21 @@
 // sum(3)(4)(5)
 
 //calculate(3)(7)(fn)
+
+const sum = (a, b, c = 0) => {
+  return a + b + c
+}
+
+const result = sum(1,2,3)
+// console.log(result)
+
+const calculate = (valueOne) => {
+  return function(valueTwo) {
+    return function(action) {
+      return action(valueOne,valueTwo)
+    }
+  }
+}
+
+const resultCalc = calculate(5)(2)(sum)
+console.log(resultCalc)
