2facfeab53cd40dde6d6f0c5188484e4262df41a
calexandredev <calexandredev@gmail.com>
2020-11-17 20:07:45 -0300

    callback hell

--- functions/PromiseTwo.js
+++ functions/PromiseTwo.js
@@ -1,3 +1,26 @@
-setTimeout(() => {
-  console.log('nanannanana nana')
-}, 2000)
+// setTimeout(() => {
+//   console.log('nanannanana nana') 
+//   setTimeout(() => {
+//     console.log('au au au au')
+//     setTimeout(() => {
+//       console.log('biro biro')
+//     }, 2000)
+//   }, 2000)
+// }, 2000)
+
+console.log('====================')
+
+const waitingFor = (time = 2000) => {
+  return new Promise( resolve => {
+    setTimeout(() => {
+      console.log('Resolving the promise...')
+      resolve('WOOOOOOOW')
+    }, time)
+  })
+}
+
+const name = waitingFor()
+  .then(() => waitingFor())
+  .then(waitingFor())
+
+console.log(name)
