527d3813a50579c2b846cdf812979319e2cc5622
calexandredev <calexandredev@gmail.com>
2020-11-17 20:43:09 -0300

    answer promise challenge

---
+++ functions/challengeThree.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+const readFile = way => {
+  return new Promise(resolve => {
+    fs.readFile(way, (error, content) => {
+      resolve(content.toString())
+    })
+    console.log('after read...')
+  })
+}
+
+const way = path.join(__dirname, 'data.txt')
+readFile(way)
+  .then(content => content.split('\n'))
+  .then(lines => lines.join(','))
+  .then(content => `The final content is: ${content}`)
+  .then(console.log)
